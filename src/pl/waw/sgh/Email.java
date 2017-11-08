654e891db11494de90d4111f5f5a5875276c05dd
dnlwlnc <daniel.welenc@gmail.com>
2017-11-08 17:03:12 +0100

    Praca domowa: sprawdzić adres e-mail

--- src/pl/waw/sgh/Email.java
+++ src/pl/waw/sgh/Email.java
@@ -1,9 +1,46 @@
 package pl.waw.sgh;
 
 public class Email {
+    static char[] specialChars = new char[] {'!', '#', '$', '%', '^', '&', '*', '(', ')', '-', '/', '~', '[', ']'} ;
+
     public static void main(String[] args) {
         final String EMAIL = "abasdasd@asaf.pl";
+        checkEmail(EMAIL);
+    }
 
-        // PROGRAM MA SPRAWDZIC POPRAWNOSC ADRESU E-MAIL; NIE UŻYWAC REGEX
+    public static void checkEmail(String email) {
+        int malpa = email.indexOf('@');
+
+        if (malpa > 0) {
+            String address = email.substring(0,malpa);
+            String domain = email.substring(malpa + 1, email.length());
+
+            if (checkAddress(address) && checkDomain(domain)){
+                System.out.println(address + "@" + domain + " jest prawidlowym adresem e-mail");
+            } else {
+                System.out.println("Adres e-mail jest nieprawidłowy");
+            }
+        }
+    }
+
+    public static boolean checkAddress(String address){
+        for (int i=0; i<specialChars.length; i++) {
+            if (address.indexOf(specialChars[i]) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkDomain(String domain){
+        if ((domain.charAt(0) != '.') && domain.indexOf('.') > 0 && domain.charAt(domain.length()-1) != '.') {
+            for (int i=0; i<specialChars.length; i++) {
+                if (domain.indexOf(specialChars[i]) > 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
     }
 }
