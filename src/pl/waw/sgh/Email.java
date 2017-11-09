cb14c4dea9f196b8dedc60b74490cbe90b6c2f59
dnlwlnc <daniel.welenc@gmail.com>
2017-11-09 10:23:47 +0100

    Poprawka: znak '@' w domenie teraz zwraca nieprawidlowy adres e-mail

--- src/pl/waw/sgh/Email.java
+++ src/pl/waw/sgh/Email.java
@@ -4,21 +4,21 @@ public class Email {
     static char[] specialChars = new char[] {'!', '#', '$', '%', '^', '&', '*', '(', ')', '-', '/', '~', '[', ']'} ;
 
     public static void main(String[] args) {
-        final String EMAIL = "abasdasd@asaf.pl";
+        final String EMAIL = "abasdasd_@asaf.pl";
         checkEmail(EMAIL);
     }
 
     public static void checkEmail(String email) {
-        int malpa = email.indexOf('@');
+        int monkey = email.indexOf('@');
 
-        if (malpa > 0) {
-            String address = email.substring(0,malpa);
-            String domain = email.substring(malpa + 1, email.length());
+        if (monkey > 0) {
+            String address = email.substring(0,monkey);
+            String domain = email.substring(monkey + 1, email.length());
 
             if (checkAddress(address) && checkDomain(domain)){
-                System.out.println(address + "@" + domain + " jest prawidlowym adresem e-mail");
+                System.out.println(" Adres " + email + " jest prawidlowym adresem e-mail.");
             } else {
-                System.out.println("Adres e-mail jest nieprawidłowy");
+                System.out.println("Adres " + email + " jest nieprawidłowy");
             }
         }
     }
@@ -33,7 +33,10 @@ public class Email {
     }
 
     public static boolean checkDomain(String domain){
-        if ((domain.charAt(0) != '.') && domain.indexOf('.') > 0 && domain.charAt(domain.length()-1) != '.') {
+        if (domain.charAt(0) != '.'
+                && domain.indexOf('.') > 0
+                && domain.charAt(domain.length()-1) != '.'
+                && domain.indexOf('@') == 0) {
             for (int i=0; i<specialChars.length; i++) {
                 if (domain.indexOf(specialChars[i]) > 0) {
                     return false;
