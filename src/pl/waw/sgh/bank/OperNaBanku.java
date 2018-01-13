ed19ec0c8997753b05cc71e6643240fc12f15428
dnlwlnc <daniel.welenc@gmail.com>
2018-01-13 13:12:42 +0100

    Bank - ciąg dalszy

--- src/pl/waw/sgh/bank/OperNaBanku.java
+++ src/pl/waw/sgh/bank/OperNaBanku.java
@@ -1,10 +1,19 @@
 package pl.waw.sgh.bank;
 
 public class OperNaBanku {
-    Bank bank = new Bank();
-    Klient k1 = bank.stworzKlienta("Andrzej", "Kowalski", "akowalski@onet.pl");
-    Klient k2 = bank.stworzKlienta("Jan", "Wisniewsi", "jwisnia@wp.pl");
-    Konto acc1 = bank.stworzKonto(true, "PLN", k1);
-    Konto acc2 = bank.stworzKonto(false, "PLN", k1);
-    Konto acc3 = bank.stworzKonto(false, "PLN", k2);
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+        Klient k1 = bank.stworzKlienta("Andrzej", "Kowalski", "akowalski@onet.pl");
+        Klient k2 = bank.stworzKlienta("Jan", "Wisniewsi", "jwisnia@wp.pl");
+        Konto acc1 = bank.stworzKonto(true, "PLN", k1);
+        Konto acc2 = bank.stworzKonto(false, "PLN", k1);
+        Konto acc3 = bank.stworzKonto(false, "PLN", k2);
+
+        // UZNANIE, OBCIĄŻENIE, PRZELEW (z konta na konto)
+        // IFELSE na sytuacje wyjatkowe -> konto nie istnieje, nie ma srodkow, itd.
+        // ZNALEZIENIE konta z listy
+
+        //
+    }
+
 }
